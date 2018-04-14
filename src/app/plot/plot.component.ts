d5145c574fa8b121fbb469a5fa977460041a77e8
Jacob Tucker <jmtucker6@crimson.ua.edu>
2018-04-14 15:20:54 -0500

    Fixes data object

--- src/app/plot/plot.component.ts
+++ src/app/plot/plot.component.ts
@@ -22,6 +22,8 @@ export class PlotComponent implements OnInit {
     this.plotService.onNewMessage().subscribe(msg => {
       console.log(msg);
       this.data = msg;
+      this.data.mode = 'markers'
+      this.data.type = 'scatter';
       Plotly.plot(this.element, [this.data], this.layout);
     });
   }
@@ -30,18 +32,10 @@ export class PlotComponent implements OnInit {
     this.element = this.el.nativeElement;
 
     this.data = {
-      alpha: {
-          "max": 0.4176603081056414,
-          "min": 0.016835995035434276,
-          "mean": 0.1966480039590039,
-          "variance": 0.004850732463785751
-      },
-      beta: {
-          "max": 0.9409940446576599,
-          "min": 0.054656648524500515,
-          "mean": 0.5056359355383458,
-          "variance": 0.03426381912385437
-      }
+      x: [0.5, 0.7, 0.9],
+      y: [0.1, 0.2, 0.3],
+      type: 'scatter',
+      mode: 'markers'
     }; 
 
     this.layout = {
